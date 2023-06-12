75af8c1b2e6c6ee2d2ee2648896ce4faf438da21
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2023-06-12 10:54:10 +0200

    adding data management

--- DeliveryShopFront/src/components/Dashboard/Seller/SellerDashboard.js
+++ DeliveryShopFront/src/components/Dashboard/Seller/SellerDashboard.js
@@ -4,13 +4,32 @@ import SellerTopBar from "./SellerTopBar";
 import SellerSideBar from "./SellerSideBar";
 import TopBar from "../Admin/AdminTopBar";
 import { Row, Col, Container } from "react-bootstrap";
+import Map from "../../Map";
+import { useState } from "react";
 
 const SellerDashboard = () => {
+  const [packages, setPackages] = useState([
+    {
+      id: 1,
+      latitude: 41.1234,
+      longitude: 20.5678,
+    },
+    {
+      id: 2,
+      latitude: 42.5678,
+      longitude: 21.1234,
+    },
+  ]);
+  const handlePackageClick = (packageId) => {
+    // Implementirajte logiku za prihvatanje paketa na osnovu ID-a
+    console.log(`Prihvaćen paket sa ID-jem: ${packageId}`);
+  };
+
   return (
     <div className="container">
       <TopBar />
       <SellerSideBar />
-
+      <Map packages={packages} handlePackageClick={handlePackageClick} />
       {/* Add seller-specific content */}
     </div>
   );
--- DeliveryShopFront/src/components/Map/index.js
+++ DeliveryShopFront/src/components/Map/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
 
 const Map = ({ packages, handlePackageClick }) => {
-  const mapboxToken = "YOUR_MAPBOX_TOKEN"; // Postavite svoj Mapbox token ovde
+  const mapboxToken =
+    "pk.eyJ1Ijoic2NvdGhpcyIsImEiOiJjaWp1Y2ltYmUwMDBicmJrdDQ4ZDBkaGN4In0.sbihZCZJ56-fsFNKHXF8YQ"; // Postavite svoj Mapbox token ovde
 
   const handleMarkerClick = (packageId) => {
     // Prosledite kliknutoj markici ID paketa na funkciju za prihvatanje paketa
@@ -13,18 +14,21 @@ const Map = ({ packages, handlePackageClick }) => {
     <ReactMapGL
       width="100%"
       height="500px"
-      latitude={YOUR_DEFAULT_LATITUDE}
-      longitude={YOUR_DEFAULT_LONGITUDE}
+      latitude={41.1234}
+      longitude={20.5678}
       zoom={10}
       mapboxApiAccessToken={mapboxToken}
     >
-      {packages.map((package) => (
+      {packages.map((packaged) => (
         <Marker
-          key={package.id}
-          latitude={package.latitude}
-          longitude={package.longitude}
+          key={packaged.id}
+          latitude={packaged.latitude}
+          longitude={packaged.longitude}
         >
-          <div className="marker" onClick={() => handleMarkerClick(package.id)}>
+          <div
+            className="marker"
+            onClick={() => handleMarkerClick(packaged.id)}
+          >
             {/* Prikaz ikonice paketa */}
           </div>
         </Marker>
